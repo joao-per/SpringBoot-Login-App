503ae26924d6a1c7435d9754b487968a1c0dd956
joao-per <rodriguespereirajoaopaulo@gmail.com>
2023-08-02 22:55:48 +0100

    Removed unnecessary files and added index for redirections

--- logintemplate/src/main/java/com/empego/logintemplate/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.empego.logintemplate.repository;
-
-import com.empego.logintemplate.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
--- logintemplate/src/main/java/com/empego/logintemplate/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.empego.logintemplate.service;
-
-import com.empego.logintemplate.model.User;
-
-public interface UserService {
-    void save(User user);
-    User findByUsername(String username);
-}
